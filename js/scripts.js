a592c42848b71271a2c9e28d2e68ebb186a1e47e
Ewa <grtxhu@gmail.com>
2018-05-08 18:38:07 +0200

    Modify files

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
+ charArray = text.split(" ", 21);
+console.log(charArray);
+console.log(charArray[0]);
+
+str = charArray.join([separator = ' '])
+console.log(str);
 var dinosaur = 'triceratops';
 var secondDinosaur = dinosaur.toUpperCase();
-var realDinosaur = text.replace('Velociraptor', secondDinosaur);
+console.log(secondDinosaur); 
+
+var realDinosaur = str.replace(charArray[0], secondDinosaur);
 
 var finalDinosaur = realDinosaur.slice(0,realDinosaur.length/2);
 console.log(finalDinosaur);
+
